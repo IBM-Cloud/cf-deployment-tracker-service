a111e58907ac3830731cbe162a1bd2a7cfe9237c
Bradley Holt <bradley.holt@us.ibm.com>
2015-05-04 16:25:18 -0400

    Accept tracking data and log to console

---
+++ app.js
@@ -0,0 +1,74 @@
+// Licensed under the Apache 2.0 License. See footer for details.
+
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    cloudant = require('cloudant'),
+    program = require('commander'),
+    dotenv = require('dotenv'),
+    bodyParser = require('body-parser');
+
+dotenv.load();
+
+var app = express();
+
+(function(app) {
+  if (process.env.VCAP_SERVICES) {
+    var vcapServices = JSON.parse(process.env.VCAP_SERVICES);
+    app.set('vcapServices', vcapServices);
+    if (vcapServices.cloudantNoSQLDB && vcapServices.cloudantNoSQLDB.length > 0) {
+      var service = vcapServices.cloudantNoSQLDB[0];
+      if (service.credentials) {
+        app.set('deployment-tracker-db', cloudant({
+          username: service.credentials.username,
+          password: service.credentials.password,
+          account: service.credentials.username
+        }));
+      }
+    }
+  }
+})(app);
+
+var jsonParser = bodyParser.json();
+// Handle POSTing an event
+app.post('/', jsonParser, function(req, res) {
+  var app = req.app;
+  var deploymentTrackerDb = app.get('deployment-tracker-db');
+  if (!deploymentTrackerDb) {
+    return res.status(500).json({ error: 'No database server configured' })
+  }
+  if (!req.body) {
+    return res.sendStatus(400);
+  }
+  console.log("Application Name: " + req.body.application_name);
+  console.log("Application Space ID: " + req.body.space_id);
+  console.log("Application Version: " + req.body.application_version);
+  console.log("Application URIs: " + req.body.application_uris.join(', '));
+  res.status(201).json({
+    ok: true
+  });
+});
+// Set the port number based on a command line switch, an environment variable, or a default value
+app.set('port', program.port || process.env.PORT || 3000);
+// Serve static assets
+app.use(express.static(path.join(__dirname, 'public')));
+// Create the HTTP server
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+//-------------------------------------------------------------------------------
+// Copyright IBM Corp. 2015
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-------------------------------------------------------------------------------
