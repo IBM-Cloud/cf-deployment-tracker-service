5d331312985a04c552e6fa40bb749fafa6fc224e
Jeff Sloyer <jbsloyer@us.ibm.com>
2015-09-14 09:40:05 -0400

    fixed code linting issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,29 @@
 module.exports = function (grunt) {
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            options : {
-                jshintrc: ".jshintrc",
-                ignores : [ "node_modules/**/*.js" ]
-            },
-            src: ["Gruntfile.js", "app.js", "admin.js"],
-        },
-        jscs: {
-            src: ["Gruntfile.js", "app.js", "admin.js"],
-            options: {
-                config: ".jscsrc",
-                esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
-                verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
-                requireCurlyBraces: [ "if" ]
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options : {
+        jshintrc: ".jshintrc",
+        ignores : [ "node_modules/**/*.js" ]
+      },
+      src: ["Gruntfile.js", "app.js", "admin.js"],
+    },
+    jscs: {
+      src: ["Gruntfile.js", "app.js", "admin.js"],
+      options: {
+        config: ".jscsrc",
+        esnext: true, // If you use ES6 http://jscs.info/overview.html#esnext
+        verbose: true, // If you need output with rule names http://jscs.info/overview.html#verbose
+        requireCurlyBraces: [ "if" ]
+      }
+    }
+  });
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
 
-    grunt.registerTask("default", [ "lint", "jscs" ]);
+  grunt.registerTask("default", [ "lint", "jscs" ]);
 
-    grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks("grunt-jscs");
 
-    grunt.registerTask("lint", "Check for common code problems.", [ "jshint" ]);
+  grunt.registerTask("lint", "Check for common code problems.", [ "jshint" ]);
 };
