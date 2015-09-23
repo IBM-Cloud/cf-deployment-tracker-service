3538117d134c4a70e8c9dfed2cc454821a45d8f0
Jeff Sloyer <jbsloyer@us.ibm.com>
2015-09-23 14:34:04 -0400

    fix styling

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
 var sessionStore;
 
